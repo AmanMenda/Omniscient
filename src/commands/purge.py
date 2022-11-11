a9d9ae928f514eff260f803b8d426793ccf9bdd0
Aman Menda <92232342+AmanMenda@users.noreply.github.com>
2022-11-11 19:55:19 +0100

    chore: .gitignore and architecture (#5)
    * chore: .gitignore and architecture
    
    * chore(workflow): trailing spaces removed
    
    * feat(moderation): kick, ban and unban members
    
    * feat(command, event): help, join & leave handled
    
    * chore: makefile

---
+++ src/commands/purge.py
@@ -0,0 +1,18 @@
+from config import bot, commands
+
+@bot.command(help="Delete a number n of messages")
+@commands.has_permissions(manage_messages=True)
+async def purge(ctx, n: int):
+    messages_history = [message async for message in ctx.channel.history(limit=n + 1)]
+    for message in messages_history:
+        await message.delete()
+    await ctx.send(f"`!purge` used by {ctx.author.mention}")
+
+@purge.error
+async def purge_error(ctx, error):
+    if isinstance(error, commands.Forbidden):
+        await ctx.send(f"You do not have proper permissions to delete the message")
+    elif isinstance(error, commands.NotFound):
+        await ctx.send(f"The message has already been deleted")
+    else:
+        await ctx.send(f"Deleting the message failed")
